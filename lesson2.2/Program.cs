c50528e4988121ce84443f834503434349426709
Grigory_Uyranev <23grisha23okay1@mail.ru>
2023-04-12 22:09:38 +0300

    Home_work2.2

---
+++ lesson2.2/Program.cs
@@ -0,0 +1,42 @@
+﻿// 2. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+Console.Write("Введите номер дня недели: ");
+string text = Console.ReadLine();
+int numberDayOfWeek = Convert.ToInt32(text);
+
+if (numberDayOfWeek == 1)
+{
+Console.WriteLine("Понедельник");
+}
+if (numberDayOfWeek == 2)
+{
+Console.WriteLine("Вторник");
+}
+if (numberDayOfWeek == 3)
+{
+Console.WriteLine("Среда");
+}
+if (numberDayOfWeek == 4)
+{
+Console.WriteLine("Четверг");
+}
+if (numberDayOfWeek == 5)
+{
+Console.WriteLine("Пятница");
+}
+if (numberDayOfWeek == 6)
+{
+Console.WriteLine("Суббота");
+}
+if (numberDayOfWeek == 7)
+{
+Console.WriteLine("Воскресенье");
+}
+
+Console.WriteLine("Какое-то сообщение");
+
+if (numberDayOfWeek < 1)
+{
+Console.WriteLine("Нет такого дня");
+}
